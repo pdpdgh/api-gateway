17f067f5de0d1ba86db3d5d103b5257993abb31b
pdpdgh <18823886787@qq.com>
2019-04-18 01:17:07 +0800

    Initial commit

---
+++ src/main/java/com/jinhuazhiguang/apigateway/ZuulConfig.java
@@ -0,0 +1,21 @@
+package com.jinhuazhiguang.apigateway;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Desc : ZuulConfig
+ * @auth : pdp
+ * @date : 2019/04/18 0:56
+ */
+//@Component
+//public class ZuulConfig {
+//
+//    @ConfigurationProperties("zuul")
+//    @RefreshScope
+//    public ZuulProperties zuulProperties() {
+//        return new ZuulProperties();
+//    }
+//}
